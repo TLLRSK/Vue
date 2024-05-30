0be441bc776f6e545698686e844e53ef6b6c86d9
TLLRSK <tullarisk@gmail.com>
2024-05-30 22:55:38 +0200

    change: app migrated to vue.js

--- 1.Getting started/app.js
+++ 1.Getting started/app.js
@@ -1,13 +1,13 @@
-const button = document.querySelector("button");
-const input = document.querySelector("#goal");
-const list = document.querySelector("ul");
-
-const addGoal = () => {
-    const enteredValue = input.value;
-    const listItem = document.createElement("li");
-    listItem.textContent = enteredValue;
-    list.appendChild(listItem);
-    input.value = "";
-}
-
-button.addEventListener("click", addGoal);
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: '',
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        }
+    },
+}).mount("#app")
