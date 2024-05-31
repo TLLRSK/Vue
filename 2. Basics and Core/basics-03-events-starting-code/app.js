b6fd0a064cd53224d8b555036841c4a7938d517e
TLLRSK <tullarisk@gmail.com>
2024-05-31 17:45:38 +0200

    add: basics 03

--- 2. Basics and Core/basics-03-events-starting-code/app.js
+++ 2. Basics and Core/basics-03-events-starting-code/app.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 5,
+      name: "",
+      confirmedName: "",
     };
   },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submitForm() {
+      this.confirmedName = this.name;
+    },
+    add() {
+      return this.counter++;
+    },
+    reduce() {
+      return this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
